98a1f988c9434458eeca70a8b661c2d791df6847
Pavel Dubrovin <dev@dev.myguest.virtualbox.org>
2025-01-13 14:34:29 +0300

    0.11_AR

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG_MODE', True)
 
-ALLOWED_HOSTS = os.getenv(('ALLOWED_HOSTS').split(', '),['localhost','127.0.0.1'])
+ALLOWED_HOSTS = os.getenv(('ALLOWED_HOSTS').split(', '),('localhost, 127.0.0.1').split(', '))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
